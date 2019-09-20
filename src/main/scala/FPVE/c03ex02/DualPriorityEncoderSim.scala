b877b33734924118508eae29124f8a96e394edb3
Gao Ya'nan <abutter.gao@gmail.com>
2019-09-20 19:02:00 +0800

    Unduplicate ffs and mask function
    And also add an object Utils for these two functions

--- src/main/scala/FPVE/c03ex02/DualPriorityEncoderSim.scala
+++ src/main/scala/FPVE/c03ex02/DualPriorityEncoderSim.scala
@@ -3,11 +3,13 @@ package FPVE.c03ex02
 import spinal.core._
 import spinal.core.sim._
 
-object PriorityEncoder12bSim {
+object Utils {
   def ffs(x : Int) : Int = 32 - Integer.numberOfLeadingZeros(x)
 
   def mask(x : Int) : Int = if (x > 0) (1 << (x - 1)) - 1 else 0
+}
 
+object PriorityEncoder12bSim {
   def main(args: Array[String]) {
     SimConfig.withWave.doSim(new PriorityEncoder12b){dut =>
       val limit = 1<<12 - 1
@@ -17,8 +19,8 @@ object PriorityEncoder12bSim {
 
         sleep(1)
 
-        assert(dut.io.encode.toInt == ffs(i))
-        assert(dut.io.mask.toInt == mask(ffs(i)))
+        assert(dut.io.encode.toInt == Utils.ffs(i))
+        assert(dut.io.mask.toInt == Utils.mask(Utils.ffs(i)))
       }
 
       sleep(1)
@@ -27,10 +29,6 @@ object PriorityEncoder12bSim {
 }
 
 object PriorityEncoder12bMuxSim {
-  def ffs(x : Int) : Int = 32 - Integer.numberOfLeadingZeros(x)
-
-  def mask(x : Int) : Int = if (x > 0) (1 << (x - 1)) - 1 else 0
-
   def main(args: Array[String]) {
     SimConfig.withWave.doSim(PriorityEncoderMux(width = 12)){dut =>
       val limit = 1<<12 - 1
@@ -40,8 +38,8 @@ object PriorityEncoder12bMuxSim {
 
         sleep(1)
 
-        assert(dut.io.encode.toInt == ffs(i))
-        assert(dut.io.mask.toInt == mask(ffs(i)))
+        assert(dut.io.encode.toInt == Utils.ffs(i))
+        assert(dut.io.mask.toInt == Utils.mask(Utils.ffs(i)))
       }
 
       sleep(1)
@@ -50,10 +48,6 @@ object PriorityEncoder12bMuxSim {
 }
 
 object DualPriorityEncoderSim {
-  def ffs(x : Int) : Int = 32 - Integer.numberOfLeadingZeros(x)
-
-  def mask(x : Int) : Int = if (x > 0) (1 << (x - 1)) - 1 else 0
-
   def main(args: Array[String]) {
     SimConfig.withWave.doSim(new DualPriorityEncoder12b){dut =>
       val limit = 1<<12 - 1
@@ -63,8 +57,8 @@ object DualPriorityEncoderSim {
 
         sleep(1)
 
-        assert(dut.io.first.toInt == ffs(i))
-        assert(dut.io.second.toInt == ffs(i & mask(ffs(i))))
+        assert(dut.io.first.toInt == Utils.ffs(i))
+        assert(dut.io.second.toInt == Utils.ffs(i & Utils.mask(Utils.ffs(i))))
       }
 
       sleep(1)
@@ -73,10 +67,6 @@ object DualPriorityEncoderSim {
 }
 
 object DualPriorityEncoderMuxSim {
-  def ffs(x : Int) : Int = 32 - Integer.numberOfLeadingZeros(x)
-
-  def mask(x : Int) : Int = if (x > 0) (1 << (x - 1)) - 1 else 0
-
   def main(args: Array[String]) {
     SimConfig.withWave.doSim(DualPriorityEncoderMux(width = 12)){dut =>
       val limit = 1<<12 - 1
@@ -86,8 +76,8 @@ object DualPriorityEncoderMuxSim {
 
         sleep(1)
 
-        assert(dut.io.first.toInt == ffs(i))
-        assert(dut.io.second.toInt == ffs(i & mask(ffs(i))))
+        assert(dut.io.first.toInt == Utils.ffs(i))
+        assert(dut.io.second.toInt == Utils.ffs(i & Utils.mask(Utils.ffs(i))))
       }
 
       sleep(1)
