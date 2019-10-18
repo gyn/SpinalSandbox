df66c12b22afbfc8183535dfddb76cec59a06350
Gao Ya'nan <abutter.gao@gmail.com>
2019-10-18 12:39:33 +0800

    Check the result
    So we don't need to check the waveform

--- src/main/scala/FPVE/c06li06/Bin2BCDSim.scala
+++ src/main/scala/FPVE/c06li06/Bin2BCDSim.scala
@@ -3,6 +3,8 @@ package FPVE.c06li06
 import spinal.core._
 import spinal.core.sim._
 
+import scala.collection.mutable.ListBuffer
+
 object Bin2BCDSim {
   def main(args: Array[String]) {
     val systemClock = 50 MHz
@@ -22,6 +24,16 @@ object Bin2BCDSim {
       sleep(resetPeriod)
       dut.clockDomain.deassertReset()
 
+      def Int2BCD(number: Int) : Int = {
+        val numberList = new ListBuffer[Int]()
+        var n = number
+        while (n != 0) {
+          numberList.prepend(n % 10)
+          n = n/10
+        }
+        numberList.fold(0)((acc, s) => acc * 16 + s)
+      }
+
       for (i <- 0 until 1 << width) {
         dut.io.n #= i
         dut.clockDomain.waitSampling(1)
@@ -30,6 +42,8 @@ object Bin2BCDSim {
         dut.clockDomain.waitSampling(width + 2)
 
         assert(dut.io.done.toBoolean)
+        val errorMessage = f"n = ${i}, output 0x${dut.io.result.toInt}%x"
+        assert(Int2BCD(i) == dut.io.result.toInt, errorMessage)
 
         dut.io.start #= false
         dut.clockDomain.waitSampling(1)
